a17a01095561c0d1d8d233a7fa9c0e7e0ec43354
siTotes <timeminecraft.1140@gmail.com>
2024-02-17 17:04:18 +0700

    meee

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 
@@ -12,3 +12,30 @@ app.listen(8999, () => {
 
 })
 
+*/
+
+const {
+    spawn
+} = require('child_process')
+const path = require('path')
+
+function runSlebeww() {
+    let args = [path.join(__dirname, 'onic.js'), ...process.argv.slice(2)]
+    console.log([process.argv[0], ...args].join('\n'))
+    let plg = spawn(process.argv[0], args, {
+            stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+        })
+        .on('message', data => {
+            if (data == 'reset') {
+                console.log('Memulai Ulang Bot...')
+                plg.kill()
+                runSlebeww()
+                delete plg
+            }
+        })
+        .on('exit', code => {
+            console.error('Keluar dengan kode:', code)
+            if (code == '.' || code == 1) setTimeout(runSlebeww, 0)
+        })
+}
+runSlebeww()
