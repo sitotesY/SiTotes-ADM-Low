37d3186117addc8ec4ddf9a2071939afce9f3a84
siTotes <timeminecraft.1140@gmail.com>
2024-02-17 11:04:31 +0700

    meee

--- onic.js
+++ onic.js
@@ -37,7 +37,7 @@ const {
 } = require("@adiwajshing/baileys")
 
 const pino = require('pino')
-const fs = require('@cyclic.sh/s3fs')
+const fs = require('@cyclic.sh/s3fs/promises') 
 
 const useStore = true
 const usePairingCode = false
